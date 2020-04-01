5ca28959acbaa9c366c68dde8f1acd93ee26fe6f
EngineerMel <ramelhaines25@gmail.com>
2020-04-01 13:33:28 -0400

    added dependencies

---
+++ Curricula-Backend/src/main/java/com/example/curricula/CurriculaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.curricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculaBackendApplication.class, args);
+	}
+
+}
---
+++ Curricula-Backend/src/test/java/com/example/curricula/CurriculaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
