b7f73300776f713c070a197f31a2cb41ed1e6e80
tasayed <taqrim.sayed@gmail.com>
2020-04-02 14:39:47 -0400

    added swagger

--- Curricula/src/main/java/com/revature/configuration/SwaggerConfiguration.java
+++ Curricula/src/main/java/com/revature/configuration/SwaggerConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-         
+
 @Configuration
 public class SwaggerConfiguration implements WebMvcConfigurer{
 
