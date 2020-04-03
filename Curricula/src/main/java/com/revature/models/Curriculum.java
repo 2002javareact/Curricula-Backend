4150584ce981e6f80adc13a2a7ec3006a453f68f
Gerard Cancino <gerard.cancino@gmail.com>
2020-04-03 17:00:55 -0400

    Update Curriculum.java

--- Curricula/src/main/java/com/revature/models/Curriculum.java
+++ Curricula/src/main/java/com/revature/models/Curriculum.java
@@ -10,8 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 @Entity
 public class Curriculum {
 	
