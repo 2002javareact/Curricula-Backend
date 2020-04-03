853f512f6eeee299aee6fc1997764505f173120c
cabre <cabrerojesus@gmail.com>
2020-04-03 13:21:26 -0400

    omitted metadata

--- Curricula/src/main/java/com/revature/controllers/SkillController.java
+++ Curricula/src/main/java/com/revature/controllers/SkillController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.Skill;
 import com.revature.services.SkillService;
-//delete me needed to push code
+
 @RestController 
 @RequestMapping("skill")
 @CrossOrigin
