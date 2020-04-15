26d44a877e02bcf3bda590a12432350cb1c93632
cabre <cabrerojesus@gmail.com>
2020-04-15 13:28:20 -0400

    cors_configuration_updated

--- Curricula/src/main/java/com/revature/configuration/CorsConfiguration.java
+++ Curricula/src/main/java/com/revature/configuration/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer{
 	public void addCorsMappings(CorsRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addMapping("/**") // all endpoints in the api
-        .allowedOrigins("http://localhost:1234", "http://localhost:3000","http://localhost:2020") // allow this origin
+        .allowedOrigins("http://localhost:1234", "http://localhost:3000","http://localhost:2020","http://project1-cabrerojesus.s3-website.us-east-2.amazonaws.com/") // allow this origin
         .allowedMethods("PUT", "DELETE", "GET", "OPTIONS", "POST", "PATCH")
         .allowedHeaders("header1", "Content-Type", "Authorization")
 //        .exposedHeaders("header1", "header2")
